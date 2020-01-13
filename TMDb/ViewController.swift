eda9fc950c67011995c1f8d4fa37069574f24774
mbilohatniuk <m.bilohatniuk@gmail.com>
2020-01-13 11:22:46 +0200

    initial commit(added empty progect)

---
+++ TMDb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TMDb
+//
+//  Created by Maksym Bilohatniuk on 1/13/20.
+//  Copyright © 2020 Maksym Bilohatniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
